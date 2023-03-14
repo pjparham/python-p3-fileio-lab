da79497da47fb9cd5ffdebb2d52452b510754883
Parker Parham <parkerparham@gmail.com>
2023-03-14 16:58:59 -0400

    Adds functionality to write to files, append files, and read files

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    file_name += '.txt'
+    with open(file_name, mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+      file_name += '.txt'
+      with open(file_name, mode='a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name += '.txt'
+    with open(file_name, encoding='utf-8') as text_file:
+        return text_file.read()
